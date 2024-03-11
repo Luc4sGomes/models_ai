c368d782883c813e18a5fc68309a17c1e471e631
Lucas Gomes <lucasgominhaa448@gmail.com>
2024-03-10 22:00:25 -0300

    added model

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import pandas as panda
 import matplotlib.pyplot as plot
 
+from sklearn.model_selection import train_test_split
+
 file = "https://raw.githubusercontent.com/rafaelmm82/ufpb-ia-20201/master/02-fundamentos_de_ia/02_aula_pratica_classificacao_iris/iris.csv"
 dataset = panda.read_csv(file, header=0)
 
 
-panda.plotting.scatter_matrix(dataset)
-plot.show()
+
+array = dataset.values
+print(type(array))
+x = array[:, 0:4]
+y = array[:, 4]
+
+print ('\n dimensão de X\n', x.shape)
+print ('\n dimensão de Y\n', y.shape)
